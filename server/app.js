d807be4f7f7fb63198c1b8a2f3aa714c81479108
Eugene Taranov <tayev021@gmail.com>
2025-06-03 21:17:34 +0300

    feat(server): add error handlers to the app

--- server/app.js
+++ server/app.js
@@ -5,6 +5,9 @@ import cors from 'cors';
 import morgan from 'morgan';
 import cookieParser from 'cookie-parser';
 
+import { AppError } from './utils/AppError.js';
+import { globalErrorHandler } from './controllers/errorController.js';
+
 const app = express();
 
 //MIDDLEWARES
@@ -12,5 +15,13 @@ app.use(cors({ origin: process.env.ORIGIN, credentials: true }));
 app.use(morgan('dev'));
 app.use(express.json()); // body parser
 app.use(cookieParser());
+
+// ROUTES
+
+app.all('/{*any}', function (req, res, next) {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+});
+
+app.use(globalErrorHandler);
 
 export { app };
