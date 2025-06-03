e89d0ab4149075ca269d9112bd265d52e346cee0
Eugene Taranov <tayev021@gmail.com>
2025-06-03 21:05:42 +0300

    feat(server): add userModelPlaceholder

---
+++ server/models/userModelPlaceholder.js
@@ -0,0 +1,61 @@
+import * as fs from 'node:fs';
+import bcrypt from 'bcryptjs';
+
+import { AppError } from '../utils/AppError.js';
+import {
+  isValidName,
+  isValidEmail,
+  isValidPassword,
+} from '../utils/validators.js';
+
+class UserModelPlaceholder {
+  constructor() {
+    this.users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'));
+  }
+
+  find(email) {
+    return { ...this.users[email] };
+  }
+
+  async isCorrectPassword(password, encryptedPassword) {
+    return await bcrypt.compare(password, encryptedPassword);
+  }
+
+  async create(userData) {
+    if (this.users[userData.email])
+      throw new AppError('This email already registered', 400);
+
+    this.validate(userData);
+
+    const newUser = {
+      name: `${userData.name[0].toUpperCase()}${userData.name.slice(1)}`,
+      surname: `${userData.surname[0].toUpperCase()}${userData.surname.slice(1)}`,
+      email: userData.email,
+      password: await bcrypt.hash(userData.password, 12),
+    };
+
+    this.users[newUser.phone] = newUser;
+    this.save();
+
+    return this.users[newUser.phone];
+  }
+
+  save() {
+    fs.writeFileSync('./data/users.json', JSON.stringify(this.users));
+  }
+
+  validate({ name, surname, email, password }) {
+    if (!isValidName(name))
+      throw new AppError(`"${name}" is not a valid name!`, 400);
+    if (!isValidName(surname))
+      throw new AppError(`"${surname}" is not a valid surname!`, 400);
+    if (!isValidEmail(email))
+      throw new AppError(`"${email}" is not a valid email!`, 400);
+    if (!isValidPassword(password))
+      throw new AppError(`"${password}" is not a valid password!`, 400);
+  }
+}
+
+const User = new UserModelPlaceholder();
+
+export { User };
