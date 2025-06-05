574395ff537fa0ac110e4ad4ab82498738ddd1e9
Eugene Taranov <tayev021@gmail.com>
2025-06-05 22:22:41 +0300

    feat: add Signup page

---
+++ client/src/pages/signup/ui/SignupPage.jsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+import Background from '../../../shared/ui/Background';
+import { Signup } from '../../../widgets/signup';
+
+const StyledSignupPage = styled(Background)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 2rem;
+`;
+
+export function SignupPage() {
+  return (
+    <StyledSignupPage>
+      <Signup />
+    </StyledSignupPage>
+  );
+}
