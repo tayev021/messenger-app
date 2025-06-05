ba89a001f8d418c5d6cd7cedbff2f9acebbe7aa5
Eugene Taranov <tayev021@gmail.com>
2025-06-05 22:33:18 +0300

    refactor: replace custom form in Signin widget to shared Form

--- client/src/widgets/signin/ui/Signin.jsx
+++ client/src/widgets/signin/ui/Signin.jsx
@@ -1,74 +1,8 @@
-import styled from 'styled-components';
 import { useState } from 'react';
 import { HiOutlineEnvelope, HiOutlineLockClosed } from 'react-icons/hi2';
 
 import { useSignin } from '../lib/hooks/useSignin';
-
-const Form = styled.form`
-  width: 32rem;
-  display: flex;
-  flex-direction: column;
-  gap: 2rem;
-  padding: 3rem;
-  border-radius: 1rem;
-  background-color: var(--color-grey-50);
-  box-shadow: var(--box-shadow-medium);
-`;
-
-const H3 = styled.h3`
-  margin-bottom: 1rem;
-  font-size: 1.8rem;
-  font-weight: 600;
-  text-align: center;
-`;
-
-const Row = styled.div`
-  position: relative;
-
-  svg {
-    width: 2rem;
-    height: 2rem;
-    position: absolute;
-    top: 0.8rem;
-    left: 1.6rem;
-    color: var(--color-grey-400);
-  }
-
-  &:has(input:focus) svg {
-    color: var(--color-sky-400);
-  }
-`;
-
-const Input = styled.input`
-  width: 100%;
-  padding: 0.6rem 1.2rem 0.6rem 5rem;
-  border: 2px solid var(--color-sky-400);
-  border-radius: 99rem;
-  box-shadow: var(--box-shadow-smallest);
-
-  &::placeholder {
-    color: var(--color-grey-400);
-  }
-`;
-
-const Button = styled.button`
-  padding: 0.6rem 1.2rem;
-  border-radius: 99rem;
-  color: var(--color-grey-50);
-  background-color: var(--color-sky-400);
-  box-shadow: var(--box-shadow-smallest);
-  transition: all 0.2s ease;
-
-  &:hover {
-    transform: translateY(-1px);
-    box-shadow: var(--box-shadow-small);
-  }
-
-  &:active {
-    transform: translateY(0);
-    box-shadow: var(--box-shadow-smallest);
-  }
-`;
+import { Form } from '../../../shared/ui/form';
 
 export function Signin() {
   const [email, setEmail] = useState('admin@gmail.com');
@@ -85,10 +19,10 @@ export function Signin() {
 
   return (
     <Form onSubmit={handleSubmit}>
-      <H3>Sign in to Messenger</H3>
-      <Row>
+      <Form.Heading>Sign in to Messenger</Form.Heading>
+      <Form.Row>
         <HiOutlineEnvelope />
-        <Input
+        <Form.Input
           type="email"
           id="email"
           disabled={isLoading}
@@ -96,10 +30,10 @@ export function Signin() {
           value={email}
           onChange={(e) => setEmail(e.target.value)}
         />
-      </Row>
-      <Row>
+      </Form.Row>
+      <Form.Row>
         <HiOutlineLockClosed />
-        <Input
+        <Form.Input
           type="password"
           id="password"
           disabled={isLoading}
@@ -107,8 +41,8 @@ export function Signin() {
           value={password}
           onChange={(e) => setPassword(e.target.value)}
         />
-      </Row>
-      <Button disabled={isLoading}>Sign in</Button>
+      </Form.Row>
+      <Form.Submit disabled={isLoading}>Sign in</Form.Submit>
     </Form>
   );
 }
