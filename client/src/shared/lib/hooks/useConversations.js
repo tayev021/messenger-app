2662d53c6cc400f6b5f960713023d4280b39ae8e
Eugene Taranov <tayev021@gmail.com>
2025-06-09 21:29:27 +0300

    feat: update useConversations to filter searched conversations

--- client/src/shared/lib/hooks/useConversations.js
+++ client/src/shared/lib/hooks/useConversations.js
@@ -1,12 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { getConversations } from '../../api/apiConversations';
+import { useSearchConversationsContext } from './useSearchConversationsContext';
 
 export function useConversations() {
   const { isLoading, data: conversations } = useQuery({
     queryKey: ['conversations'],
     queryFn: getConversations,
   });
+  const { search } = useSearchConversationsContext();
 
-  return { isLoading, conversations };
+  let searchedConversations = [];
+
+  if (search) {
+    searchedConversations = conversations.filter(
+      ({ partnerName, partnerSurname }) => {
+        const title = `${partnerName} ${partnerSurname}`.toLowerCase();
+        return title.includes(search.toLowerCase());
+      }
+    );
+  } else {
+    searchedConversations = conversations;
+  }
+
+  return { isLoading, conversations: searchedConversations };
 }
