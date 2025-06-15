67ce113cd496a88b6e3f27798f5d07dd88deacf4
Eugene Taranov <tayev021@gmail.com>
2025-06-15 20:26:59 +0300

    feat(server): update ConversationModelPlaceholder

--- server/models/conversationModelPlaceholder.js
+++ server/models/conversationModelPlaceholder.js
@@ -26,6 +26,36 @@ class ConversationModelPlaceholder {
     return this.conversations[id];
   }
 
+  updateLastMessage({ conversationId, user, message }) {
+    if (message.length > 25) {
+      this.conversations[conversationId].lastMessage =
+        message.slice(0, 22) + '...';
+    } else {
+      this.conversations[conversationId].lastMessage = message;
+    }
+
+    for (let [userId, unwatchedNum] of Object.entries(
+      this.conversations[conversationId].unwatched
+    )) {
+      if (userId !== user.id) {
+        this.conversations[conversationId].unwatched[userId] = unwatchedNum + 1;
+      }
+    }
+
+    this.save();
+
+    return this.conversations[conversationId];
+  }
+
+  watchMessage({ conversationId, user }) {
+    if (this.conversations[conversationId].unwatched[user.id] <= 0) return;
+
+    this.conversations[conversationId].unwatched[user.id]--;
+    this.save();
+
+    return this.conversations[conversationId];
+  }
+
   save() {
     fs.writeFileSync(
       './data/conversations.json',
