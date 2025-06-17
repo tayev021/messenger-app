f97a91f5f07e1cff4771d3bf7ba26f5c8d16b691
Eugene Taranov <tayev021@gmail.com>
2025-06-17 21:05:20 +0300

    feat(server): add new controllers to chat router

--- server/routes/chatRoutes.js
+++ server/routes/chatRoutes.js
@@ -5,12 +5,16 @@ import {
   createMessage,
   watchMessage,
   getChat,
+  uploadImages,
+  resizeImages,
 } from '../controllers/chatController.js';
 
 const chatRouter = express.Router();
 
 chatRouter.route('/:chatId').get(protect, getChat);
-chatRouter.route('/:chatId/message').post(protect, createMessage);
+chatRouter
+  .route('/:chatId/message')
+  .post(protect, uploadImages, resizeImages, createMessage);
 chatRouter
   .route('/:chatId/message/:messageId/watch')
   .post(protect, watchMessage);
