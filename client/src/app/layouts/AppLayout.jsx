332179d794ea130e0f09a7af16957246a708838b
Eugene Taranov <tayev021@gmail.com>
2025-06-20 21:26:52 +0300

    feat: add UserPanel to AppLayout

--- client/src/app/layouts/AppLayout.jsx
+++ client/src/app/layouts/AppLayout.jsx
@@ -3,9 +3,11 @@ import { Outlet } from 'react-router';
 
 import { Sidebar } from '../../widgets/sidebar';
 import { Background } from '../../shared/ui/Background';
+import { UserPanel } from '../../widgets/userPanel';
 
 const StyledAppLayout = styled.div`
   height: 100vh;
+  position: relative;
   display: grid;
   grid-template-columns: min-content 1fr;
   grid-template-rows: 1fr;
@@ -14,6 +16,7 @@ const StyledAppLayout = styled.div`
 export function AppLayout() {
   return (
     <StyledAppLayout>
+      <UserPanel />
       <Sidebar />
       <Background as="main">
         <Outlet />
