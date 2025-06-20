9801153bdb6ed9f71d058b25fd4bba0cddf7d773
Eugene Taranov <tayev021@gmail.com>
2025-06-20 21:08:22 +0300

    feat(server): add new user routes

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,19 +1,29 @@
 import express from 'express';
 
 import {
-  logout,
   protect,
   signin,
+  signout,
   signup,
 } from '../controllers/authController.js';
-import { getMe } from '../controllers/userController.js';
+import {
+  uploadAvatar,
+  resizeAvatar,
+  changeAvatar,
+  changePassword,
+  getMe,
+} from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.route('/signup').post(signup);
 userRouter.route('/signin').post(signin);
-userRouter.route('/logout').get(logout);
+userRouter.route('/signout').get(signout);
 
 userRouter.route('/me').get(protect, getMe);
+userRouter.route('/password').patch(protect, changePassword);
+userRouter
+  .route('/avatar')
+  .patch(protect, uploadAvatar, resizeAvatar, changeAvatar);
 
 export { userRouter };
