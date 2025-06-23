198257b8eab2a19aa03c43236c9059ffdde29357
Eugene Taranov <tayev021@gmail.com>
2025-06-23 06:53:23 +0300

    fix: remove unnecessary notification to user

--- client/src/app/routes/ProtectedRoute.jsx
+++ client/src/app/routes/ProtectedRoute.jsx
@@ -1,23 +1,18 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router';
-import toast from 'react-hot-toast';
 
 import { useUser } from '../../shared/lib/hooks/useUser';
 import { PageLoading } from '../../pages/pageLoading';
 
 export function ProtectedRoute({ children }) {
   const navigate = useNavigate();
-  const { isLoading, isError, error, user } = useUser();
+  const { isLoading, error, user } = useUser();
 
   useEffect(
     function () {
-      if (isError) {
-        toast.error(`${error}`);
-      }
-
       if (!isLoading && !user) navigate('signin');
     },
-    [isLoading, isError, error, user, navigate]
+    [isLoading, error, user, navigate]
   );
 
   if (isLoading) return <PageLoading />;
