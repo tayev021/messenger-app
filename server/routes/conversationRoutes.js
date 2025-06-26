f7ec0d56d870b20db8aeca37177c61b6e120110c
Eugene Taranov <tayev021@gmail.com>
2025-06-26 22:24:06 +0300

    feat(server): add new routes conversationRoutes

--- server/routes/conversationRoutes.js
+++ server/routes/conversationRoutes.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
 import { protect } from '../controllers/authController.js';
-import { getConversations } from '../controllers/conversationController.js';
+import {
+  getConversations,
+  startConversation,
+} from '../controllers/conversationController.js';
 
 const conversationRouter = express.Router();
 
 conversationRouter.route('/').get(protect, getConversations);
+conversationRouter.route('/').post(protect, startConversation);
 
 export { conversationRouter };
