3eeabcaae302eff554474463735487374199a780
Eugene Taranov <tayev021@gmail.com>
2025-06-30 13:02:47 +0300

    feat: add link to go back from SignupPage

--- client/src/pages/signup/ui/SignupPage.jsx
+++ client/src/pages/signup/ui/SignupPage.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router';
 
 import { Background } from '../../../shared/ui/Background';
 import { Signup } from '../../../widgets/signup';
@@ -11,10 +12,36 @@ const StyledSignupPage = styled(Background)`
   gap: 2rem;
 `;
 
+const Footer = styled.footer`
+  width: 32rem;
+  padding: 2rem;
+  border-radius: 1rem;
+  background-color: var(--color-grey-50);
+  box-shadow: var(--box-shadow-medium);
+`;
+
+const P = styled.p`
+  text-align: center;
+`;
+
+const StyledLink = styled(Link)`
+  border-bottom: 2px solid transparent;
+  color: var(--color-sky-500);
+
+  &:hover {
+    border-color: var(--color-sky-500);
+  }
+`;
+
 export function SignupPage() {
   return (
     <StyledSignupPage>
       <Signup />
+      <Footer>
+        <P>
+          Have an Account? <StyledLink to="/signin">Signin!</StyledLink>
+        </P>
+      </Footer>
     </StyledSignupPage>
   );
 }
